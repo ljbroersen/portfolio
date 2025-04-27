1494e43a3ad3771ffc07cf2e45aad0884d8954be
Lisa Broersen <lj.broersen@gmail.com>
2025-04-27 22:37:39 +0900

    feat: created components for each topic

---
+++ src/components/IntroductionCard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="border-[rgb(var(--color-primary))] border-1">
+        <div class="">
+      <h1 class="text-3xl font-bold text-[rgb(var(--color-primary))]">Software Engineer</h1>
+        <p>There is text here</p>
+        <p>Here too</p>
+    </div>
+    </div>
+</template>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
-
+import IntroductionCard from "./IntroductionCard.vue"
 </script>
 
 <template>
   <div class="min-h-screen flex justify-center bg-[rgb(var(--color-bg-primary))]">
     <div class="w-1/2 bg-[rgb(var(--color-bg-secondary))]">
-      <div class="flex flex-col border-[rgb(var(--color-primary))] border-1">
-      <h1 class="text-3xl font-bold text-[rgb(var(--color-primary))]">Software Engineer</h1>
-
-      <div class="mt-4">
-        <p>There is text here</p>
-      </div>
-
-      <div class="mt-4 text-sm">
-        <p>Here too</p>
-      </div>
-    </div>
-    <div class="flex flex-col border-[rgb(var(--color-primary))] border-1">
-      <h1 class="text-3xl font-bold text-[rgb(var(--color-primary))]">Software Engineer</h1>
-
-      <div class="mt-4">
-        <p>There is text here</p>
-      </div>
-
-      <div class="mt-4 text-sm">
-        <p>Here too</p>
-      </div>
-    </div>
+      <IntroductionCard />
   </div>
   </div>
 </template>
