708d46eac71a8417b04f7278fef6d5070dcf7f3d
Lisa Broersen <lj.broersen@gmail.com>
2025-05-04 00:23:16 +0900

    feat: added a navbar

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
 import AboutMeCard from "./AboutMeCard.vue";
 import Footer from "./Footer.vue";
+import Navbar from "./Navbar.vue";
 // import IntroductionCard from "./IntroductionCard.vue"
 import ProjectsCard from "./ProjectsCard.vue";
 import SkillsCard from "./SkillsCard.vue";
 </script>
 
 <template>
-  <div class="min-h-screen flex justify-center bg-[rgb(var(--color-bg-primary))]">
-    <div class="w-1/2 bg-[rgb(var(--color-bg-secondary))]">
+  <div class="min-h-screen flex justify-center bg-[rgb(var(--color-bg-primary))] text-[rgb(var(--color-text-base))]">
+      <Navbar />
+    <div class="w-1/2 bg-[rgb(var(--color-bg-secondary))] rounded-lg shadow">
       <!-- <IntroductionCard /> -->
-      <AboutMeCard />
-      <ProjectsCard />
-      <SkillsCard />
-      <Footer />
-     </div>
+      <section id="about">
+        <AboutMeCard />
+      </section>
+      <section id="projects">
+        <ProjectsCard />
+      </section>
+      <section id="skills">
+        <SkillsCard />
+      </section>
+      <section id="footer">
+        <Footer />
+      </section>
+    </div>
   </div>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-30 p-4">
+      <nav class="flex flex-col space-y-2">
+        <a href="#about" class="hover:underline text-sm">About Me</a>
+        <a href="#projects" class="hover:underline text-sm">Projects & Engagement</a>
+        <a href="#skills" class="hover:underline text-sm">Tech Skills</a>
+      </nav>
+    </div>
+</template>
