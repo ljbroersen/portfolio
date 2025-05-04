401fe1092de20af8dc7dc693803ee48d6b6581e8
Lisa Broersen <lj.broersen@gmail.com>
2025-05-04 22:41:50 +0900

    fix: changed the navbar settings. It's now fixed in position

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-30 p-4">
-      <nav class="flex flex-col space-y-2">
+      <nav class="flex flex-col fixed space-y-2 w-1/16">
         <a href="#about" class="hover:underline text-sm">About Me</a>
         <a href="#projects" class="hover:underline text-sm">Projects & Engagement</a>
         <a href="#skills" class="hover:underline text-sm">Tech Skills</a>
